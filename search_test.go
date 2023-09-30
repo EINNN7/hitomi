fe7f7fdf53a1a88f6393dc1e0e8babe56d7a3a07
delta <delta@deltalab.dev>
2023-09-30 23:38:31 +0900

    bug: fixed test

--- search_test.go
+++ search_test.go
@@ -7,16 +7,16 @@ import (
 var search *Search
 
 func TestSearch_SearchNode(t *testing.T) {
-	node, err := search.NodeByAddress("global", 0)
+	node, err := search.NodeByAddress("female", 0)
 	if err != nil {
 		t.Fatal(err)
 	}
-	data, err := search.SearchNode("global", HashTerm("big"), node)
+	data, err := search.SearchNode("female", HashTerm("big"), node)
 	if err != nil {
 		t.Fatal(err)
 	}
 	t.Log(data)
-	tags, err := search.TagSuggestionData("global", data)
+	tags, err := search.TagSuggestionData("female", data)
 	if err != nil {
 		t.Fatal(err)
 	}
